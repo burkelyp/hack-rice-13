c02fe0ca6e06b09eebe02107c50c1eccf8ac26c1
Burkely Pettijohn <pettijob@oregonstate.edu>
2023-09-24 08:39:27 -0500

    update README, docstrings, clean directory

--- bot/bot.py
+++ bot/bot.py
@@ -1,3 +1,18 @@
+# Author: Burkely Pettijohn and Nikita Naumov
+# GitHub username: burkelyp BummerBobby
+# Date: 9/22-24/2023
+# Description:  Rice Hack-a-thon 13: HackRice 13
+#               We have built a Discord bot that manages media files sent
+#               to a discord server. Once the bot joins a server it manages
+#               the url's of images and videos posted to the discord. It
+#               then tracks how often a member of the discord interacts with
+#               the media via likes, laughing, etc. reactions. You can then
+#               call the !spotlight function to display the top n media posts
+#               n defaults to 5 but can be specified to any number.
+
+
+
+
 import os
 import csv
 import discord
@@ -6,9 +21,9 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')   # store token in .env file
 
-datapath = Path(os.path.dirname(__file__))
+datapath = Path(os.path.dirname(__file__))    # for writing the url data to /storage.csv
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -16,25 +31,39 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-	print(f'Bot {bot.user} is online! Id: {bot.user.id}')
+    """
+    Notify user when bot is online
+    """
+    print(f'Bot {bot.user} is online! Id: {bot.user.id}')
 
-@bot.event
-async def on_message(message):
-    # Ignore messages sent by the bot itself to prevent a loop
-    if message.author == bot.user:
-        return
+# @bot.event
+# async def on_message(message):
+#     """
+#     This was a test function to get the bot to say hello to us upon asking   
+#     """
+#     # Ignore messages sent by the bot itself to prevent a loop
+#     if message.author == bot.user:
+#         return
 
-    # Check if the message mentions the bot
-    if bot.user.mentioned_in(message):
-        # Respond with "hello"
-        await message.channel.send("Hellooo!")
+#     # Check if the message mentions the bot
+#     if bot.user.mentioned_in(message):
+#         # Respond with "hello"
+#         await message.channel.send("Hellooo!")
 
 @bot.command(name='hello')
 async def say_hello(ctx):
+    """
+    To say hello to our bot, just type <!hello>
+    """
     await ctx.send("Hello!")
     
 @bot.event
 async def on_message(message):
+    """
+    This function collects the url of every image or video posted to the discord,
+    and stores it in media_storage.csv
+    The function also notifies the discord that someone has posted media
+    """
     # Ignore messages sent by the bot itself to prevent a loop
     if message.author == bot.user:
         return
@@ -55,6 +84,9 @@ async def on_message(message):
 
 @bot.event
 async def on_reaction_add(reaction, user):
+    """
+    Adds url to media_storage.csv to count
+    """
     # Check if the reaction is on an image or video message
     if reaction.message.attachments:
         for attachment in reaction.message.attachments:
@@ -65,6 +97,14 @@ async def on_reaction_add(reaction, user):
 
 @bot.command(name='spotlight')
 async def spotlight(ctx, n: int = 5):
+    """
+    !spotlight function to display the top n media posts to the group
+
+    example:
+    !spotlight 7      the top 7 media 
+    !spotlight        the top 5 media
+
+    """
     media_data = str(Path(os.path.dirname(__file__))) + '/media_storage.csv'
 
     count_dict = {}
